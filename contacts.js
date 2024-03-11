dfb90d515ccab2cee39de5161a5cef63a822368f
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-03-11 13:35:49 +0200

    corrected readme

--- contacts.js
+++ contacts.js
@@ -1,7 +1,7 @@
 const fs = require(fs / promises);
 const path = require("path");
 
-const contactsPath = path / join(_dirname, "contacts.json");
+const contactsPath = path / join(__dirname, "contacts.json");
 
 async function listContacts() {
   // ...твій код. Повертає масив контактів.
